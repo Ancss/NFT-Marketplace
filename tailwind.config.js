17e79d45e694317b4d6a6fdcf74cbde1077b5cfe
Ancss <61501274+Ancss@users.noreply.github.com>
2024-05-15 16:46:26 +0800

    update tailwind variable

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        icons: "#5ddee2",
+        "icons-light": "#5ddee257",
+        "icons-bg": "#5ddee2",
+        "shadow-dark": "#373939",
+        "bg-shadow-dark": "#373939",
+        "shadow-light": "#1b181e",
+        "main-bg": "#1b181e",
+      },
+      boxShadow: {
+        custom: "1px 1px 2px #5ddee2, -1px -1px 2px #5ddee2",
+        "custom-2": "1px 1px 2px #5ddee2, -1px -1px 2px #5ddee2",
+      },
+    },
   },
   plugins: [],
 };
