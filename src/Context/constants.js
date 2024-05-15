ed893175efda0be16bfe7d7c977fae4f62dd17a6
Ancss <61501274+Ancss@users.noreply.github.com>
2024-05-15 16:46:03 +0800

    add  abi and address

---
+++ src/Context/constants.js
@@ -0,0 +1,6 @@
+import nftMarketplace from "./NFTMarketplace.json";
+
+//NFT MARKETPLACE
+export const NFTMarketplaceAddress =
+  "0x61975F663adDF71B9a172f29882eBBec56d5463E";
+export const NFTMarketplaceABI = nftMarketplace.abi;
