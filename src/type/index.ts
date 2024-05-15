9983c27b546eabb1d51d8337215bb07853f06f3f
Ancss <61501274+Ancss@users.noreply.github.com>
2024-05-15 16:45:02 +0800

    create Nft marketplace item type

---
+++ src/type/index.ts
@@ -0,0 +1,12 @@
+export type MarketItem = {
+  tokenId: number;
+  seller: string;
+  owner: string;
+  price: number;
+  sold: boolean;
+  image: string;
+  name: string;
+  description: string;
+  tokenURI: string;
+
+};
