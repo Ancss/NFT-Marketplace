c72f106a64206f5c9862672fea4b4be42ca32674
Ancss <61501274+Ancss@users.noreply.github.com>
2024-05-15 16:06:10 +0800

    add images

---
+++ src/img/index.ts
@@ -0,0 +1,122 @@
+const collection = "/images/collection.png";
+const creatorbackground1 = "/images/creatorbackground-1.jpeg";
+const creatorbackground2 = "/images/creatorbackground-2.jpeg";
+const creatorbackground3 = "/images/creatorbackground-3.jpeg";
+const creatorbackground4 = "/images/creatorbackground-4.jpg";
+const creatorbackground5 = "/images/creatorbackground-5.jpg";
+const creatorbackground6 = "/images/creatorbackground-6.jpg";
+const creatorbackground7 = "/images/creatorbackground-7.jpg";
+const creatorbackground8 = "/images/creatorbackground-8.jpg";
+const creatorbackground9 = "/images/creatorbackground-9.jpg";
+const creatorbackground10 = "/images/creatorbackground-10.jpg";
+const creatorbackground11 = "/images/creatorbackground-11.jpg";
+const user1 = "/images/user-1.png";
+const user2 = "/images/user-2.png";
+const user3 = "/images/user-3.png";
+const user4 = "/images/user-4.png";
+const user5 = "/images/user-5.png";
+const user6 = "/images/user-6.png";
+const user7 = "/images/user-7.png";
+const user8 = "/images/user-8.png";
+const user9 = "/images/user-9.png";
+const user10 = "/images/user-10.png";
+const service1 = "/images/service-1.png";
+const service2 = "/images/service-2.png";
+const service3 = "/images/service-3.png";
+const service4 = "/images/service-4.png";
+const logoLight = "/images/logo-light.svg";
+const logo = "/images/logo.svg";
+const facebook = "/images/facebook.svg";
+const twitter = "/images/twitter.svg";
+const telegram = "/images/telegram.svg";
+const youtube = "/images/youtube.svg";
+const vimeo = "/images/vimeo.svg";
+const vector = "/images/VectorHIW.svg";
+const zgcmq = "/images/zgcmq.svg";
+const hero = "/images/hero.png";
+const nft_1 = "/images/nft_1.png";
+const getAble = "/images/getable.png";
+const nft_image_1 = "/images/nft-image-1.png";
+const nft_image_2 = "/images/nft-image-2.png";
+const nft_image_3 = "/images/nft-image-3.png";
+const musiceWave = "/images/musicwave.png";
+const update = "/images/update.png";
+const earn = "/images/eran.png";
+const NFTVideo = "/images/nftvideo.png";
+const hero2 = "/images/hero2.png";
+const upload = "/images/upload.png";
+const provider1 = "/images/provider-1.png";
+const provider2 = "/images/provider-2.png";
+const provider3 = "/images/provider-3.png";
+const provider4 = "/images/provider-4.png";
+const founder1 = "/images/founder1.jpg";
+const founder2 = "/images/founder2.jpg";
+const founder3 = "/images/founder3.jpg";
+const founder4 = "/images/founder4.jpg";
+const loader = "/images/loadergif.gif";
+const errorgif = "/images/error.gif";
+const transfer = "/images/transfer.gif";
+const transferEther = "/images/ethereTransfer.gif";
+
+
+export default {
+  hero2,
+  founder1,
+  founder2,
+  founder3,
+  founder4,
+  provider1,
+  provider2,
+  provider3,
+  provider4,
+  upload,
+  NFTVideo,
+  earn,
+  collection,
+  update,
+  musiceWave,
+  nft_image_1,
+  nft_image_2,
+  nft_image_3,
+  getAble,
+  nft_1,
+  hero,
+  creatorbackground1,
+  creatorbackground2,
+  creatorbackground3,
+  creatorbackground4,
+  creatorbackground5,
+  creatorbackground6,
+  creatorbackground7,
+  creatorbackground8,
+  creatorbackground9,
+  creatorbackground10,
+  creatorbackground11,
+  user1,
+  user2,
+  user3,
+  user4,
+  user5,
+  user6,
+  user7,
+  user8,
+  user9,
+  user10,
+  service1,
+  service2,
+  service3,
+  service4,
+  logo,
+  logoLight,
+  facebook,
+  telegram,
+  twitter,
+  vector,
+  vimeo,
+  youtube,
+  zgcmq,
+  loader,
+  errorgif,
+  transfer,
+  transferEther,
+};
