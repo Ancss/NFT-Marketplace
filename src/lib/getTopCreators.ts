de272e835ec186b71d1cf5346d7c3c3067ff1023
Ancss <61501274+Ancss@users.noreply.github.com>
2024-05-21 16:31:51 +0800

    update utils lib

---
+++ src/lib/getTopCreators.ts
@@ -0,0 +1,27 @@
+import { TMarketItem } from "@/type";
+
+export const getTopCreators = (creators: TMarketItem[]) => {
+  const finalCreators: {
+    seller: string,
+    total: number
+  }[] = [];
+
+  const finalResults = creators.reduce((index, currentValue) => {
+    (index[currentValue.seller] = index[currentValue.seller] || []).push(
+      currentValue
+    );
+
+    return index;
+  }, {} as { [key in string]: TMarketItem[] });
+
+  Object.entries(finalResults).forEach((item) => {
+    const seller = item[0];
+    const total = item[1]
+      .map((newItem) => Number(newItem.price))
+      .reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+
+    finalCreators.push({ seller, total});
+  });
+
+  return finalCreators;
+};
